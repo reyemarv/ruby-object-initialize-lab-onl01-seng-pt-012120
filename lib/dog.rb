9a5cec00e4cbd5ceaa2738e37bc7b4f6f50b1b0e
Marvin Reyes <reyemarv@gmail.com>
2020-02-07 21:10:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,15 @@
 class Dog 
   
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
    @name = name
-   unless breed.empty?
-   @breed 
- else breed = "Mutt"
- end 
+  @breed = breed 
   end 
   
   def name
     @name
   end 
   
-  def breed(new_breed)
-     if new_breed = @breed 
-       @breed
-     else new_breed = "Mutt"
-     end 
+  def breed
+     @breed 
    end 
 end 
